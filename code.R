e51e27361e5bad4a9245ddfd8b5fbe12bf88d8b0
Lau Jue Hua <laujuehua@gmail.com>
2020-12-22 21:27:51 +0800

    test2

---
+++ code.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+
+#local files and GitHub repo will not be synced automatically
+
+#working directory > staging area > local repo > upstream repo
+#pull changes from remote repo(github)
+#stage (add) files to the local repo
+#commit changes to local repo
+#push changes to remote repo
+
+#in git terminal,
+#yellow > git knows nothing about the file
+# to sync, we need to add file, then commit change, then push to local repo
+#green > added 
+# dont ahve to add all files in local repo to github
+# only dont want to share
+# add suffix gitignore for files you dont want to keep track of
+# these will stop appearing in Git pane
+#commiting a change
+# whenever we commit a change, we are required to enter a comment describing the change being committed
